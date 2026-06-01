d5c457432c289c8a68775beb13afbb64d9fb1592
Diana <ddmitrieva098@example.com>
2026-06-01 19:39:57 +1100

    создано подтверждение электронной почты

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,13 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'mailing:mailing_list'
 LOGOUT_REDIRECT_URL = 'mailing:mailing_list'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
