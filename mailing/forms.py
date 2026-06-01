f0a93f3b5c93cff1b5d455ef69010872d2b524ee
Diana <ddmitrieva098@example.com>
2026-06-01 19:50:15 +1100

    создано поле «Владелец» для моделей «Получатель» и «Рассылка», настройки автозаполнения

--- mailing/forms.py
+++ mailing/forms.py
@@ -28,7 +28,7 @@ class MessageForm(forms.ModelForm):
 class MailingForm(forms.ModelForm):
     class Meta:
         model = Mailing
-        exclude = ("first_sending",)
+        exclude = ("first_sending", "owner")
 
     def __init__(self, *args, **kwargs):
         super(MailingForm, self).__init__(*args, **kwargs)
