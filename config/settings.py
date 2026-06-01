ebec1f4af3292903cfcfabd256d74e4fdfa23786
Diana <ddmitrieva098@example.com>
2026-06-01 17:09:18 +1100

    создание директории mailing

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'mailing',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
---
+++ mailing/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from mailing.apps import MailingConfig
+
+
+app_name = MailingConfig.name
+
+urlpatterns = [
+
+]
